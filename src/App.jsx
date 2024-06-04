46139ff2a569cdee834d76fa967ecb20ed622fda
rajaNayak123 <nayakraja151@gmail.com>
2024-06-04 13:37:31 +0530

    change images

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,14 @@ function App() {
           </div>
         </div>
           <div className='starIcon1'>
-            <img src="src/assets/Star 1.png" alt="" />
+            <img src="src/assets/Star 1 1.26.23 PM.png" alt="" />
           </div>
           <div className='starIcon2'>
-            <img src="src/assets/Star 1.png" alt="" />
+            <img src="src/assets/Star 8.png" alt="" />
           </div>
         <div className='container'>
           <div className='img'>
-            <img src="src/assets/Group 35930.png" alt="" />
+            <img src="src/assets/Group 35930 1.26.42 PM.png" alt="" />
           </div>
           <div className='textCon'>
             <h1>Make The Best <br /> Financial Decision</h1>
@@ -50,19 +50,19 @@ function App() {
         </div>
         <div className='container2'>
           <div className='blurImg'>
-            <img src="src/assets/Group 35930.png" alt="" />
+            <img src="src/assets/Group 35930 1.26.42 PM.png" alt="" />
           </div>
           <div className='ellipse'>
             <img src="src/assets/Group 35887.png" alt="" />
           </div>
           <div className='iphone1'>
-            <img src="src/assets/iPhone-13-Pro-Front(1).png" alt="" />
+            <img src="src/assets/iPhone-13-Pro-Front 1.26.34 PM.png" alt="" />
           </div>
           <div className='iphone2'>
-            <img src="src/assets/iPhone-13-Pro-Front.png" alt="" />
+            <img src="src/assets/iPhone-13-Pro-Front(1).png" alt="" />
           </div>
           <div className='iphone3'>
-            <img src="src/assets/iPhone-13-Pro-Front.png" alt="" />
+            <img src="src/assets/iPhone-13-Pro-Front(2).png" alt="" />
           </div>
         </div>
       </div>
